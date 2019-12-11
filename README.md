4c2de1ad0f8218eb18d4ed4459ee1718d70f58f7
Dan Kiss <flowfast47@gmail.com>
2019-12-10 19:37:18 -0800

    complete code challenge to find a name

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Type in 'tom jackson' in the input box to see him found.
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -5,22 +5,96 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilterableContactTable contacts={CONTACTS} />
     </div>
   );
 }
 
 export default App;
+
+class ContactRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.contact.name}</td>
+        <td>{this.props.contact.phone}</td>
+        <td>{this.props.contact.email}</td>
+      </tr>
+    );
+  }
+}
+class ContactTable extends React.Component {
+  render() {
+    var rows = [];
+    this.props.contacts.forEach((contact, index) => {
+      rows.push(<ContactRow contact={contact} key={index}/>);
+    });
+    return (
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+class SearchBar extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Search..."
+            onChange={this.props.filterInfo}
+          />
+        </form>
+
+      </div>
+
+    );
+  }
+}
+class FilterableContactTable extends React.Component {
+  state = {
+    matchFinal: []
+  }
+
+  filterInfo = (e) => {
+    let filterMatch = e.target.value;
+    // Tom
+    console.log(filterMatch)
+    let contacts = this.props.contacts;
+    this.setState({ matchFinal: contacts.filter(element => element.name.toLowerCase() === filterMatch.toLowerCase()) })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Filterable React List</h1>
+        <SearchBar contacts={CONTACTS}
+          filterInfo={this.filterInfo} />
+        <ContactTable
+          contacts={this.state.matchFinal}
+        />
+
+      </div>
+    );
+  }
+}
+var CONTACTS = [
+  { name: 'Tom Jackson', phone: '555-444-333', email: 'tom@gmail.com' },
+  { name: 'Mike James', phone: '555-777-888', email: 'mikejames@gmail.com' },
+  { name: 'Janet Larson', phone: '555-222-111', email: 'janetlarson@gmail.com' },
+  { name: 'Clark Thompson', phone: '555-444-333', email: 'clark123@gmail.com' },
+  { name: 'Emma Page', phone: '555-444-333', email: 'emma1page@gmail.com' },
+];
+
